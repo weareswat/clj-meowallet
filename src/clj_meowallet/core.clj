e121226d61c2ee8429e6d62876d870f7801753d4
Marcos <marcoslamuria@gmail.com>
2016-05-16 12:06:17 +0100

    add fn to add body to the request

--- src/clj_meowallet/core.clj
+++ src/clj_meowallet/core.clj
@@ -1,5 +1,6 @@
 (ns clj-meowallet.core
-  (:require [environ.core :refer [env]]))
+  (:require [environ.core :refer [env]]
+            [aleph.http :as http]))
 
 (def sandbox-url "https://services.sandbox.meowallet.pt/api/v2/")
 (def production-url "https://services.wallet.pt/api/v2/")
@@ -11,7 +12,20 @@
     :else sandbox-url))
 
 (defn authentify
-  [data http-ops]
-  (if-let [token (:token data)]
+  [credentials http-ops]
+  (if-let [token (:token credentials)]
     (assoc http-ops :headers {"Authorization: WalletPT " token})
     http-ops))
+
+(defn add-body
+  [data http-ops]
+  (if-let [body (:body data)]
+    (assoc http-ops :body body)
+    http-ops))
+
+(def mb-ref-url "mb/pay")
+
+(defn generate-mb-ref
+  [credentials data]
+
+  )
--- test/clj_meowallet/core_test.clj
+++ test/clj_meowallet/core_test.clj
@@ -8,7 +8,24 @@
            (core/url-builder)))))
 
 (deftest authentify-test
-  (let [data {:token "qweqweqwk"}
-        header {"Authorization: WalletPT " (:token data)}
-        result (core/authentify data {})]
+  (let [credentials {:token "qweqweqwk"}
+        header {"Authorization: WalletPT " (:token credentials)}
+        result (core/authentify credentials {})]
     (is (= header (:headers result)))))
+
+(deftest add-body-test
+  (let [data {:body {:payment {:client {:name "John Santos"
+                                        :email "johnsantos@mail.com"
+                                        :address {:country "pt"
+                                                  :address "Av. Fontes Pereira de Melo"
+                                                  :city "Lisboa"}}
+                               :amount 229
+                               :currency "EUR"
+                               :items [{:ref 123
+                                        :name "InvoiceXpress plan"
+                                        :descr "New subscription to invoicexpress"
+                                        :qt 1}]
+                               :ext_invoiceid "C12423324"}}}
+        result (core/add-body data {})]
+    (is (= (:body data)
+           (:body result)))))
