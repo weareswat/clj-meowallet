c8383b0ad348d89ffb86f3d0e5ff4db3362bf48a
Marcos <marcoslamuria@gmail.com>
2016-05-16 16:09:34 +0100

    Add generate mb ref endpoint
    Motivation
      We should be able to POST a request to `/api/v2/mb/pay` to generate an
      mb reference.
    
    Modifications
      added a function called `generate-mb-ref` which receives the following
      params:
        - credentials: `{:token "123h12kjhjjkj"}`
        - data: {:amount 10
                 :currency "EUR"
                 :expires "2014-11-13T15:59:58+0000"}
    
    Added clj-meowallet.http namespace to deal with http stuff.

---
+++ src/clj_meowallet/http.clj
@@ -0,0 +1,52 @@
+(ns clj-meowallet.http
+  (:require [cheshire.core :as json]
+            [clojure.core.async :refer [chan <!! >!! close! go <! timeout]]
+            [manifold.deferred :as d]
+            [aleph.http :as http]))
+
+(defn- retry?
+  "Verifies that the given error response is the final one, or that
+  we should try it again."
+  [data response]
+  (and (instance? java.util.concurrent.TimeoutException response)
+       (not= 0 (int (:retries data)))))
+
+(def final-response? (comp not retry?))
+
+(defn- prepare-response
+  "Handles post-response"
+  [data response]
+  (merge {:status (:status response)
+          :requests (inc (:requests data))}
+         (json/parse-string (slurp (:body response)) true)))
+
+(defn- prepare-error
+  "Handles post-response errors"
+  [data response]
+  (-> response
+      (assoc :error (str "Error getting " (:url data)))
+      (assoc :requests (inc (:requests data)))
+      (assoc :status (-> response :data :cause))
+      (assoc :body-data (slurp (-> response :data :body)))))
+
+(defn fetch-response
+  "Fetches the response for a given URL"
+  [data]
+  (let [result-ch (or (:result-ch data) (chan 1))
+        async-stream ((:method-fn data) (:url data) (:http-opts data))]
+    (d/on-realized async-stream
+                   (fn [x]
+                     (>!! result-ch (prepare-response data x))
+                     (close! result-ch))
+                   (fn [x]
+                     (if (final-response? data x)
+                       (do
+                         (>!! result-ch (prepare-error data x))
+                         (close! result-ch))
+                       (go
+                         (<! (timeout (* 300 (+ 1 (int (:requests data))))))
+                         (fetch-response (-> data
+                                             (assoc :result-ch result-ch)
+                                             (update :retries dec)
+                                             (update :requests inc)))))))
+    result-ch))
--- test/clj_meowallet/core_test.clj
+++ test/clj_meowallet/core_test.clj
@@ -2,16 +2,17 @@
   (:require [clojure.test :refer :all]
             [clj-meowallet.core :as core]))
 
-(deftest url-builder-test
+(deftest build-url-test
   (testing "sandbox url"
     (is (= core/sandbox-url
-           (core/url-builder)))))
+           (core/host)))))
 
-(deftest authentify-test
+(deftest add-headers-test
   (let [credentials {:token "qweqweqwk"}
-        header {"Authorization: WalletPT " (:token credentials)}
-        result (core/authentify credentials {})]
-    (is (= header (:headers result)))))
+        headers {"Content-Type:" "application/json"
+                 "Authorization: WalletPT " (:token credentials)}
+        result (core/add-headers credentials {})]
+    (is (= headers (:headers result)))))
 
 (deftest add-body-test
   (let [data {:body {:payment {:client {:name "John Santos"
@@ -29,3 +30,43 @@
         result (core/add-body data {})]
     (is (= (:body data)
            (:body result)))))
+
+(deftest prepare-data-test
+  (let [credentials {:token "qweqweqwk"}
+        data {:body {:payment {:client {:name "John Santos"
+                                        :email "johnsantos@mail.com"
+                                        :address {:country "pt"
+                                                  :address "Av. Fontes Pereira de Melo"
+                                                  :city "Lisboa"}}
+                               :amount 229
+                               :currency "EUR"
+                               :items [{:ref 123
+                                        :name "InvoiceXpress plan"
+                                        :descr "New subscription to invoicexpress"
+                                        :qt 1}]
+                               :ext_invoiceid "C12423324"}}}
+        path core/mb-ref-url
+        method :post
+        result (core/prepare-data credentials data path method)]
+
+      (testing "host"
+        (is (= (core/host)
+               (:host result))))
+
+      (testing "retries"
+        (is (= 2
+               (:retries result))))
+
+      (testing "url"
+        (is (= (str (core/host) path)
+               (:url result))))
+
+      (testing "request-method"
+        (is (= method
+               (:request-method result))))
+
+      (testing "http-ops"
+        (is (not (nil? (:http-opts result)))))
+
+      (testing "method-fn"
+        (is (not (nil? (:method-fn result)))))))
